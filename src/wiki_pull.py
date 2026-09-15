dc39e6ac21f0b406b6164683139ebcb231e62342
Joshua Heo <jhheo1@uci.edu>
2026-09-14 23:00:37 -0700

    fixed pandas error with io module

--- src/wiki_pull.py
+++ src/wiki_pull.py
@@ -2,18 +2,20 @@
 import requests
 # abbreviation
 import pandas as pd
+# to convert html string to file-like object
+import io
 
 wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 # header to get past 403 error
 # header adds meta data to the request (not bot traffic)
 # non-browser like strings work for wikipedia
-headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+headers = {"User-Agent": "plsgivemedata"}
 
 wiki_pull = requests.get(wiki_url, headers=headers)
 
-wiki_pull.status_code
-
-
 html_string = wiki_pull.text
 
-print(pd.read_html(html_string))
+# newer versions of pandas requires file-like object to read html tables
+html_file = io.StringIO(html_string)
+
+print(pd.read_html(html_file))
