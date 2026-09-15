476ead1350dcdadbb6929718659dabea13041c94
Joshua Heo <jhheo1@uci.edu>
2026-09-14 22:48:59 -0700

    pandas bug returning html

--- src/wiki_pull.py
+++ src/wiki_pull.py
@@ -1,12 +1,19 @@
 # wikipedia needed for company selection and sector info
 import requests
+# abbreviation
+import pandas as pd
 
 wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 # header to get past 403 error
 # header adds meta data to the request (not bot traffic)
 # non-browser like strings work for wikipedia
-headers = {"User-Agent": "plsletmegetthisdata"}
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
 wiki_pull = requests.get(wiki_url, headers=headers)
 
-print(wiki_pull.status_code)
+wiki_pull.status_code
+
+
+html_string = wiki_pull.text
+
+print(pd.read_html(html_string))
