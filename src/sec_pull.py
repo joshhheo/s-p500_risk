9fc34c1665bc95e383e349538b7fc34218cf09bf
Joshua Heo <jhheo1@uci.edu>
2026-09-15 19:03:36 -0700

    api test

--- src/sec_pull.py
+++ src/sec_pull.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from edgar import set_identity, Company
 
 # writes environment variables to os environment
 load_dotenv()
@@ -7,3 +8,6 @@ load_dotenv()
 # accesses variable from os environment
 user_agent = os.getenv("SEC_user_agent")
 
+set_identity(user_agent)
+
+print(Company("AAPL").get_financials().balance_sheet())
