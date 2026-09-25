0283adfa0fc1fb4f60a881c791c7502514454987
Joshua Heo <jhheo1@uci.edu>
2026-09-24 19:40:36 -0700

    loaded company table and financial table to neon

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Use SEC EDGAR to compute Leverage Ratio, Interest Coverage Ratio, and Current Ratio
 
 Analyze financial risk for equity investors and creditos
+
+
+Some companies did not make their total liabilties accessible in the SEC EDGAR 
+
+
--- src/create_tables.py
+++ src/create_tables.py
@@ -8,17 +8,28 @@ connection_string = os.getenv("db_connection_string")
 engine = create_engine(connection_string)
 
 # store queries as string
+# primary key to avoid repeating constants
 create_companies_table = """
-CREATE TABLE comapnies(
+CREATE TABLE companies (
     ticker TEXT PRIMARY KEY,
     company_name TEXT,
     sector TEXT
-    );
+);
 """
 
-# with statement automatically closes connnection
+create_financials_table = """
+CREATE TABLE financials (
+    ticker TEXT REFERENCES companies(ticker),
+    fiscal_period_end DATE,
+    debt_to_assets_ratio NUMERIC,
+    liabilities_method TEXT
+);
+"""
+
+# with statement to close connection automatically
 with engine.connect() as connection:
     connection.execute(text(create_companies_table))
+    connection.execute(text(create_financials_table))
     connection.commit()
-
+ 
 print("finished")
